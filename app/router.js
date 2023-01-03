efb41594429e65a52460317b8f332990208baeb6
Chris McNeese <christophermcneese@mac.com>
2023-01-02 18:40:31 -0600

    save changes

--- app/router.js
+++ app/router.js
@@ -6,8 +6,8 @@ const Router = EmberRouter.extend({
   rootURL: config.rootURL
 });
 
-Router.map(function() {
-  this.route('About');
+Router.map(function () {
+  this.route('about');
 });
 
 export default Router;
